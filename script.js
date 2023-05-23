7f9ea3943c5551ea79c62fcdb9a700dece21dbe0
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-23 23:45:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+    let name = document.getElementById('name').value;
+	let name2 = document.getElementById('name2').value;
+	alert(name + " "+ name2);
 }
